de3e7370990fb1af8349dd7a98945337d28650da
kimheeae <110895079+hheeaaee@users.noreply.github.com>
2022-08-23 09:51:52 +0900

    rosemary
    로즈마리 여성병원 리뉴얼

---
+++ rosemary/banner.js
@@ -0,0 +1,19 @@
+// var banUl=document.querySelector(".ban_ul");
+// var banCon=document.getElementsByClassName("ban_con");
+// var currentIndex=0;
+// var banConLen=banCon.length;
+// function slide(index){
+    
+//     banCon[index].classList.add('on');
+//     currentIndex=index;
+// }
+// function AutoSlide(){
+//     setInterval(function(){
+//         banCon[currentIndex].classList.remove('on');
+//         var CountIndex=(currentIndex+1)%banConLen;
+//         slide(CountIndex);
+        
+//     },4000);
+// }
+// slide(0);
+// AutoSlide();

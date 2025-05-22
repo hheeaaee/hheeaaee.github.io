b84e00ae58c8b5716760c3fc8ce793040aae8dc7
kimheeae <110895079+hheeaaee@users.noreply.github.com>
2025-05-22 17:10:04 +0900

    Add files via upload

--- sunghunheeae/js/js.js
+++ sunghunheeae/js/js.js
@@ -4,3 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
     thumbnail: true
   });
 });
+
+function playVideo() {
+  const videoBox = document.getElementById('videoBox');
+  videoBox.innerHTML = `
+    <iframe 
+      width="100%" height="225" 
+      src="https://www.youtube.com/embed/kLiQ3RPdDtE?autoplay=1"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen>
+    </iframe>`;
+}

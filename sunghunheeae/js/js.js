8b22587d97a8472a5abb280aa4718c5c3d3bdd5e
kimheeae <110895079+hheeaaee@users.noreply.github.com>
2025-05-23 11:42:11 +0900

    Add files via upload

--- sunghunheeae/js/js.js
+++ sunghunheeae/js/js.js
@@ -16,3 +16,40 @@ function playVideo() {
       allowfullscreen>
     </iframe>`;
 }
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => alert("계좌번호가 복사되었습니다!"))
+    .catch(() => alert("복사에 실패했어요 😢"));
+}
+
+function copyWeddingUrl() {
+  const url = window.location.href;
+  navigator.clipboard.writeText(url)
+    .then(() => alert("청첩장 주소가 복사되었습니다!"))
+    .catch(() => alert("복사에 실패했습니다 😢"));
+}
+
+function shareKakao() {
+  Kakao.Share.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: '재진 & 정은, 결혼 소식을 전합니다 💍',
+      description: '2025.06.28 SAT',
+      imageUrl: 'https://hheeaaee.github.io/sunghunheeae/image/ogthumb.jpg', // 공유 이미지
+      link: {
+        mobileWebUrl: window.location.href,
+        webUrl: window.location.href
+      }
+    },
+    buttons: [
+      {
+        title: '청첩장 보기',
+        link: {
+          mobileWebUrl: window.location.href,
+          webUrl: window.location.href
+        }
+      }
+    ]
+  });
+}
